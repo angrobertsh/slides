cae173ad6cafe87e1a5eba7795e011a9c7c80f0a
Robert Ang <angrobertsh@gmail.com>
2017-06-05 18:39:53 -0700

    first commit

---
+++ home.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+})
+
+const init = () => {
+  const left = document.getElementById("car-left-button");
+  const right = document.getElementById("car-right-button");
+  const container = document.getElementById("car-content-container");
+  const numElements = document.getElementsByClassName("car-content").length;
+  let width = document.getElementById("car-content-container").offsetWidth*-1;
+  let active = 0;
+  let str = "";
+
+  right.addEventListener("click", (e) => {
+    if(active >= numElements-1){
+      return;
+    } else {
+      left.style.display = "block";
+      active += 1;
+      container.style.transform = "translateX(" + (active*width).toString() + "px)";
+      if(active === numElements - 1){
+        right.style.display = "none";
+      }
+    }
+  })
+
+  left.addEventListener("click", (e) => {
+    if(active <= 0){
+      return;
+    } else {
+      right.style.display = "block";
+      active -= 1;
+      container.style.transform = "translateX(" + (active*-500).toString() + "px)";
+      if(active === 0){
+        left.style.display = "none";
+      }
+    }
+  })
+
+}
